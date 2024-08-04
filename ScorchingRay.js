c339b0d0366077506de00e4dc999713135a81b26
Abel Philips <31525042+abelph@users.noreply.github.com>
2024-08-04 00:42:24 -0700

    Update ScorchingRay.js
    Formatting change

--- ScorchingRay.js
+++ ScorchingRay.js
@@ -22,30 +22,30 @@ new Dialog({
 
 function scorching(num) {
 	num++;
-    const results_html = `<h2>Scorching Ray</h2>`;
+  const results_html = `<h2>Scorching Ray</h2>`;
 
-    ChatMessage.create({
-        user: game.user._id,
-        speaker: ChatMessage.getSpeaker({token: actor}),
-        content: results_html
-    });
+  ChatMessage.create({
+      user: game.user._id,
+      speaker: ChatMessage.getSpeaker({token: actor}),
+      content: results_html
+  });
 
-    for (let i = 1; i < num; i++) {
-        let shot = 'Shot ' + i;
-        ChatMessage.create({
-            user: game.user._id,
-            speaker: ChatMessage.getSpeaker({token: actor}),
-            content: shot
-        });
-        let toHit = new Roll("1d20+12").evaluate({async: false});   //Change attack modifier here
-        toHit.toMessage({
-        rollMode: 'roll',
-        speaker: {alias: name}
-        });
-        let roll = new Roll("{1d6, 2}kh1 + {1d6, 2}kh1").evaluate({async: false});  //Change damage calculation here
-        roll.toMessage({
-        rollMode: 'roll',
-        speaker: {alias: name}
-        }); 
-    }
+  for (let i = 1; i < num; i++) {
+      let shot = 'Shot ' + i;
+      ChatMessage.create({
+          user: game.user._id,
+          speaker: ChatMessage.getSpeaker({token: actor}),
+          content: shot
+      });
+      let toHit = new Roll("1d20+12").evaluate({async: false});   //Change attack modifier here
+      toHit.toMessage({
+      rollMode: 'roll',
+      speaker: {alias: name}
+      });
+      let roll = new Roll("{1d6, 2}kh1 + {1d6, 2}kh1").evaluate({async: false});  //Change damage calculation here
+      roll.toMessage({
+      rollMode: 'roll',
+      speaker: {alias: name}
+      }); 
+  }
 }
