bc16b3464b90e754bfd1589c99984e5d94105b88
Abel Philips <31525042+abelph@users.noreply.github.com>
2024-08-04 00:30:52 -0700

    Added dialogue box to scorching ray script that sets number of rays

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A small collection of macros for Foundry VTT that I've made. Minor changes may b
 ## Scripts
 ### Chimera.js
 This is based off of an item called the Chimera made by the Mithral Canvas and found [here](https://www.reddit.com/r/DnDHomebrew/comments/1218hxr/ocart_the_chimera_firearm_the_mithral_canvas_5e/).
+### ScorchingRay.js
+This script prompts you with a dialog box for the number of rays you would like to shoot. It then rolls an attack and damage roll for each ray. The script is currently set up for a character with a +12 attack modifier and elemental adept feat. The dice formulas can be changed at the commented lines.
--- ScorchingRay.js
+++ ScorchingRay.js
@@ -1,26 +1,51 @@
-const results_html = `<h2>Scorching Ray</h2>`
+new Dialog({
+    title:'Scorching Ray',
+    content:`
+      <form>
+        <div class="form-group">
+          <label>Number of Rays</label>
+          <input type='number' name='inputField'></input>
+        </div>
+      </form>`,
+    buttons:{
+      yes: {
+        icon: "<i class='fas fa-check'></i>",
+        label: `Cast`,
+        callback: html =>{
+            let result = html.find('input[name=\'inputField\']');
+            let numShots = parseInt(result.val());
+            scorching(numShots);
+        }
+      }},
+    default:'yes'
+  }).render(true);
 
-ChatMessage.create({
-    user: game.user._id,
-    speaker: ChatMessage.getSpeaker({token: actor}),
-    content: results_html
-});
+function scorching(num) {
+	num++;
+    const results_html = `<h2>Scorching Ray</h2>`;
 
-for (let i = 1; i < 6; i++) {
- let shot = 'Shot ' + i;
- ChatMessage.create({
-    user: game.user._id,
-    speaker: ChatMessage.getSpeaker({token: actor}),
-    content: shot
- });
- let toHit = new Roll("1d20+11").evaluate({async: false});
- toHit.toMessage({
- rollMode: 'roll',
- speaker: {alias: name}
-});
- let roll = new Roll("{1d6, 2}kh1 + {1d6, 2}kh1").evaluate({async: false});
- roll.toMessage({
- rollMode: 'roll',
- speaker: {alias: name}
-}); 
+    ChatMessage.create({
+        user: game.user._id,
+        speaker: ChatMessage.getSpeaker({token: actor}),
+        content: results_html
+    });
+
+    for (let i = 1; i < num; i++) {
+        let shot = 'Shot ' + i;
+        ChatMessage.create({
+            user: game.user._id,
+            speaker: ChatMessage.getSpeaker({token: actor}),
+            content: shot
+        });
+        let toHit = new Roll("1d20+12").evaluate({async: false});   //Change attack modifier here
+        toHit.toMessage({
+        rollMode: 'roll',
+        speaker: {alias: name}
+        });
+        let roll = new Roll("{1d6, 2}kh1 + {1d6, 2}kh1").evaluate({async: false});  //Change damage calculation here
+        roll.toMessage({
+        rollMode: 'roll',
+        speaker: {alias: name}
+        }); 
+    }
 }
